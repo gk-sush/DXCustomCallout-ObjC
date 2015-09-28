13f8fd899bca53ef10e72f6e6a651fbca5f9bbff
adicrescenzo <angeldicrescenzo@gmail.com>
2015-09-28 11:42:13 +0200

    update

--- Example/Pods/Target Support Files/Pods-DXCustomCallout-ObjC/Pods-DXCustomCallout-ObjC-environment.h
+++ Example/Pods/Target Support Files/Pods-DXCustomCallout-ObjC/Pods-DXCustomCallout-ObjC-environment.h
@@ -9,6 +9,6 @@
 // DXCustomCallout-ObjC
 #define COCOAPODS_POD_AVAILABLE_DXCustomCallout_ObjC
 #define COCOAPODS_VERSION_MAJOR_DXCustomCallout_ObjC 0
-#define COCOAPODS_VERSION_MINOR_DXCustomCallout_ObjC 1
+#define COCOAPODS_VERSION_MINOR_DXCustomCallout_ObjC 2
 #define COCOAPODS_VERSION_PATCH_DXCustomCallout_ObjC 0
 
